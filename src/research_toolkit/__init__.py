0b077194c653734179d2ed30bbb90faa13f0d91a
cove labs <covelabs2024@gmail.com>
2026-06-01 11:27:40 +0900

    Update package initialization

--- src/research_toolkit/__init__.py
+++ src/research_toolkit/__init__.py
@@ -1,3 +1,18 @@
-"""Research Toolkit Template."""
+"""Quant Investor research toolkit."""
 
 __version__ = "0.1.0"
+
+from .checklist import build_checklist_markdown, save_checklist
+from .config import describe_project_structure, ensure_project_folders
+from .experiment_log import ExperimentLog, create_example_log, save_experiment_log
+
+__all__ = [
+    "__version__",
+    "build_checklist_markdown",
+    "save_checklist",
+    "describe_project_structure",
+    "ensure_project_folders",
+    "ExperimentLog",
+    "create_example_log",
+    "save_experiment_log",
+]
